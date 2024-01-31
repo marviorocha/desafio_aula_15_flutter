18bb417cfffa152ddaf6559f1ccd8f0c17beaf22
Marvio Rocha <marviorocha@marviorocha.com>
2024-01-31 18:40:38 -0300

    Update card

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -26,20 +25,18 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         backgroundColor: Colors.red,
         body: SafeArea(
           child: Expanded(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                
-                const CircleAvatar(
+                CircleAvatar(
                   radius: 80,
                   foregroundImage: NetworkImage('https://picsum.photos/200'),
                 ),
-                const Text(
+                Text(
                   'Marvio Rocha',
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
@@ -47,72 +44,60 @@ class HomePage extends StatelessWidget {
                       fontSize: 30,
                       color: Colors.white),
                 ),
-                const Text(
+                Text(
                   'FLUTTER DEVELOPER',
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
                       color: Colors.white),
                 ),
-                 
-                Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                SizedBox(
+                  height: 20,
+                  width: 400,
+                  child: Divider(
                     color: Colors.white,
                   ),
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 150),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  
-                  child: const Row(
-                    children: [
-                      Icon(
-                        Icons.phone,
-                        size: 25.0,
-                        color: Colors.red,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('11 99999-9999',
+                ),
+                Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 150),
+                  child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: ListTile(
+                        title: Text(
+                          '(11) 99999-9999',
                           style: TextStyle(
                               color: Colors.red,
                               fontWeight: FontWeight.bold,
-                              fontSize: 20)),
-                    ],
-                  ),
+                              fontSize: 18),
+                        ),
+                        leading: Icon(
+                          Icons.phone,
+                          size: 25.0,
+                          color: Colors.red,
+                        ),
+                      )),
                 ),
-              
-              Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.white,
-                  ),
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 150),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  
-                  child: const Row(
-                    children: [
-                      Icon(
-                        Icons.email_outlined,
-                        size: 25.0,
-                        color: Colors.red,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text('exemple@marviorocha.com',
+                Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 150),
+                  child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: ListTile(
+                        title: Text(
+                          'marviorocha@marviorocha.com',
                           style: TextStyle(
                               color: Colors.red,
                               fontWeight: FontWeight.bold,
-                              fontSize: 18)),
-                    ],
-                  ),
+                              fontSize: 18),
+                        ),
+                        leading: Icon(
+                          Icons.email_outlined,
+                          size: 25.0,
+                          color: Colors.red,
+                        ),
+                      )),
                 ),
-            
               ],
             ),
           ),
